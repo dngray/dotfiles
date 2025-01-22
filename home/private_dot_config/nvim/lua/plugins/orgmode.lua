e240452ee84f0f9961bd6a5d00731440a05971b2
Daniel Gray <dngray@polarbear.army>
2025-01-22 23:32:12 +0000

    Rebased: 2025-01-23
    fish: update fisher
    starship: fix zlib error
    asdf: asdf script add other plugins
    nvim: add colorscheme
    nvim: add build script
    ubuntu: use vlan 50 on br0 vlan aware bridge create br51
    theme: catppuccin on sway, waybar, foot, dunst
    nheko: don't make shortcuts
    udev: add keyboard rules
    firefox: use sidebar instead of extension v136
    firefox: keep history across firefox resets v136
    firefox: use flatpak when on secureblue
    flatpak: switch away from appimages use flatpak for bitwarden
    profile: blue, container-user
    isync: rename junk, trash because of stalwart
    asdf: switch to v0.16+ rewrite
    fastfetch: update
    meslo: update
    executable_inactive-windows-transparency: update
    vdirsyncer, isync: use keyvalue pairs
    mail: use key value
    ssh: update KexAlgorithms
    neovim: add org mode plugin

---
+++ home/private_dot_config/nvim/lua/plugins/orgmode.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-orgmode/orgmode",
+  event = "VeryLazy",
+  config = function()
+    -- Setup orgmode
+    require("orgmode").setup({
+      org_agenda_files = "~/orgfiles/**/*",
+      org_default_notes_file = "~/orgfiles/refile.org",
+    })
+  end,
+}
