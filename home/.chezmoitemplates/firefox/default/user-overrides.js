0ccdb73c150cf8e8d4528945dcd11e94b3cd9dc6
Daniel Gray <dngray@polarbear.army>
2025-01-22 23:32:12 +0000

    Rebased: 2025-01-23
    fish: update fisher
    starship: fix zlib error
    asdf: asdf script add other plugins
    nvim: add colorscheme
    nvim: add build script
    ubuntu: use vlan 50 on br0 vlan aware bridge create br51
    theme: catppuccin on sway, waybar, foot, dunst
    nheko: don't make shortcuts
    udev: add keyboard rules
    firefox: use sidebar instead of extension v136
    firefox: keep history across firefox resets v136
    flatpak: switch away from appimages use flatpak for bitwarden
    profile: blue, container-user
    isync: rename junk, trash because of stalwart
    asdf: switch to v0.16+ rewrite
    fastfetch: update
    meslo: update
    executable_inactive-windows-transparency: update

--- home/.chezmoitemplates/firefox/default/user-overrides.js
+++ home/.chezmoitemplates/firefox/default/user-overrides.js
@@ -3,35 +3,36 @@ user_pref("_user.js.parrot", "overrides section syntax error");
 
 /* override recipe: enable session restore ***/
 user_pref("browser.startup.page", 3); // 0102
-  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
-  // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
-user_pref("privacy.clearOnShutdown.history", false); // 2811
-  // user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
-user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // [FF128+] [DEFAULT: true]
+
+/* keep tabs and history between reboots */
+user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false);
+user_pref("privacy.clearHistory.formdata", true);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
+user_pref("privacy.clearOnShutdown_v2.downloads", true);
+user_pref("privacy.clearOnShutdown_v2.formdata", true);
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false);
+user_pref("privacy.clearSiteData.formdata", true);
 
 /* Use resistFingerprinting on 128 */
 user_pref("privacy.resistFingerprinting", true);
-user_pref("privacy.resistFingerprinting.letterboxing", true); // optional
-user_pref("webgl.disabled", true); // optional
-user_pref("privacy.spoof_english", 2); // optional
-   // ^ I have en-US app lang and a non-matching en-** OS
-  //  so my locale without spoof_english is the same as OS which is not desirable
+user_pref("privacy.resistFingerprinting.letterboxing", true);
+user_pref("webgl.disabled", true);
+user_pref("privacy.spoof_english", 2);
 
 /* Disable DoH */
 user_pref("network.trr.mode", 5); // 0710
 
 /* APPEARANCE ***/
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
 user_pref("browser.tabs.inTitlebar", 0); // Disable title bar
+user_pref("sidebar.verticalTabs", true); // [FF136+] [DEFAULT: false]
+// user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
 
 /* UX FEATURES ***/
 user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
 
 /* 9001: Turn off previews when tabbing ***/
 user_pref("browser.ctrlTab.recentlyUsedOrder", false);
-/* 9002: Lazy loading: Tridactyl cannot capture key presses until web pages are loaded. ***/
-user_pref("browser.sessionstore.restore_tabs_lazily", false);
-/* 9003: Enable root certs for local CA ***/
+/* 9002: Enable root certs for local CA ***/
 user_pref("security.enterprise_roots.enabled", true);
 
 /* Disable the Privacy-Preserving Attribution
